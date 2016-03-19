b4a07f1ddbb3c26714518e81820aad34e47fb458
Till Harbaum <till@harbaum.org>
2016-03-19 20:40:00 +0100

    Close apps with power button

--- board/fischertechnik/TXT/rootfs/opt/ftc/txt.py
+++ board/fischertechnik/TXT/rootfs/opt/ftc/txt.py
@@ -1,5 +1,37 @@
+import struct
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+# TXT values
+INPUT_EVENT_DEVICE = "/dev/input/event1"
+INPUT_EVENT_CODE = 116
+
+# test on PC: F1 on (my) keyboard
+#INPUT_EVENT_DEVICE = "/dev/input/event17"
+#INPUT_EVENT_CODE = 59
+
+INPUT_EVENT_FORMAT = 'llHHI'
+INPUT_EVENT_SIZE = struct.calcsize(INPUT_EVENT_FORMAT)
+
+# background thread to monitor power button event device
+class ButtonThread(QThread):
+    def __init__(self):
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+ 
+    def run(self):
+        in_file = open(INPUT_EVENT_DEVICE, "rb")
+        event = in_file.read(INPUT_EVENT_SIZE)
+        while event:
+            (tv_sec, tv_usec, type, code, value) = struct.unpack(INPUT_EVENT_FORMAT, event)
+            print type, code, value
+            if type == 1 and code == INPUT_EVENT_CODE and value == 0:
+                self.emit( SIGNAL('power_button_released()'))   
+            event = in_file.read(INPUT_EVENT_SIZE)
+        return
+    
 
 # The TXTs window title bar
 class TxtTitle(QLabel):
@@ -18,6 +50,11 @@ class TxtTitle(QLabel):
         self.close.move(200,8)
 
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        # start thread to monitor power button
+        self.buttonThread = ButtonThread()
+        self.connect( self.buttonThread, SIGNAL("power_button_released()"), parent.close )
+        self.buttonThread.start()
 
 # The TXT does not use windows. Instead we just paint custom 
 # toplevel windows fullscreen
