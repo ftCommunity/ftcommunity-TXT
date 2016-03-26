5b4b659a165de39ae7723a043309399fdb0fa077
Till Harbaum <till@harbaum.org>
2016-03-26 20:11:57 +0100

    Camera app working

--- board/fischertechnik/TXT/rootfs/opt/ftc/apps/camera_test/camera_test.py
+++ board/fischertechnik/TXT/rootfs/opt/ftc/apps/camera_test/camera_test.py
@@ -5,38 +5,61 @@
 import sys, numpy, cv2
 from TxtStyle import *
 
+WIDTH=320
+HEIGHT=(WIDTH*3/4)
+FPS=10
+
 class CamWidget(QWidget):
     def __init__(self, parent=None):
 
         super(CamWidget, self).__init__(parent)
-
+        
+        # initialize camera
         self.cap = cv2.VideoCapture(0)
-        # if(!cap.isOpened())  // check if we succeeded
-        #   return -1;
+        if self.cap.isOpened():
+            self.cap.set(3,WIDTH)
+            self.cap.set(4,HEIGHT)
+            self.cap.set(5,FPS)
 
         timer = QTimer(self)
         timer.timeout.connect(self.update)
-        timer.start(1000)
+        timer.start(1000/FPS)
 
-    def grab(self):
+        qsp = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        qsp.setHeightForWidth(True)
+        self.setSizePolicy(qsp)
 
-        self.frame = self.cap.read()[1]
-        # self.frame = cv2.imread("cat.jpg")
+    def sizeHint(self):
+        return QSize(WIDTH,HEIGHT)
+
+    def heightForWidth(self,w):
+        return w*3/4
         
-        self.cvImage = cv2.resize(self.frame, (240,180))
+    def grab(self):
+        self.frame = self.cap.read()[1]
+
+        # expand/shrink to widget size
+        wsize = (self.size().width(), self.size().height())
+        self.cvImage = cv2.resize(self.frame, wsize)
 
         height, width, byteValue = self.cvImage.shape
         bytes_per_line = byteValue * width
 
         cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
-        self.mQImage = QImage(self.cvImage, width, height, bytes_per_line, QImage.Format_RGB888)
+        self.mQImage = QImage(self.cvImage, width, height,
+                              bytes_per_line, QImage.Format_RGB888)
 
     def paintEvent(self, QPaintEvent):
-        #        print "paint", self.width(), self.height()
-        self.grab()
         painter = QPainter()
         painter.begin(self)
-        painter.drawImage(0, 0, self.mQImage)
+
+        if not self.cap.isOpened():
+            painter.drawText(QRect(QPoint(0,0), self.size()),
+                             Qt.AlignCenter, "No camera");
+        else:
+            self.grab()
+            painter.drawImage(0,0,self.mQImage)
+            
         painter.end()
 
 
@@ -48,7 +71,15 @@ class FtcGuiApplication(TxtApplication):
         self.w = TxtWindow("Camera")
 
         self.cw = CamWidget()
-        self.w.setCentralWidget(self.cw)
+        
+        if False:
+            self.w.setCentralWidget(self.cw)
+        else:
+            vbox = QVBoxLayout()
+            vbox.addStretch()
+            vbox.addWidget(self.cw)
+            vbox.addStretch()
+            self.w.centralWidget.setLayout(vbox)
         
         self.w.show()
         self.exec_()        
