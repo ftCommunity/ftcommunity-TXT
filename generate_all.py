b2c0253cccf79ee4ba66c76c1bb0bfaf7275fc62
Raphael Jacob <r.jacob2002@gmail.com>
2016-03-27 23:05:16 +0200

    Fix bug same

--- generate_all.py
+++ generate_all.py
@@ -1,132 +1,71 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-
-import ConfigParser
+#!/usr/bin/python
+import os
+import sys
+import time
 import getpass
-import cgi
-import zipfile
-import sys, os
-import socket
 
-print "Content-Type: text/html"
-print ""
-print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
-print '<html xmlns="http://www.w3.org/1999/xhtml">'
-print '<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
-print '<title>fischertechnik TXT community firmware</title>'
-print '<link rel="stylesheet" href="txt.css" />'
-print '<link rel="icon" href="/favicon.ico" type="image/x-icon" />'
-print '</head><body>'
-print '<h1><div class="outline"><font color="red">fischer</font><font color="#046ab4">technik</font>&nbsp;<font color="#fcce04">TXT</font></div></h1>'
- 
-upload_dir = "/tmp"
-appbase = "/opt/ftc/apps"
-
-print "<h1>Upload</h1>"
-
-def save_uploaded_file():
-    form = cgi.FieldStorage()
-    if not form.has_key("appfile"):
-        return False,"No appfile"
-
-    fileitem = form["appfile"]
-    if not fileitem.file or not fileitem.filename:
-        return False,"No valid appfile"
-
-    filename = os.path.join(upload_dir, fileitem.filename)
-    if os.path.splitext(filename)[1] != ".zip":
-        return False,"Not a zip file"
-
-    print "Writing file to " + filename + "<br/>"
-    fout = file(filename, 'wb')
-    while 1:
-        chunk = fileitem.file.read(4096)
-        if not chunk: break
-        fout.write (chunk)
-    fout.close()
-        
-    return True,filename
-
-def unzip_uploaded_file(zip_name, appdir):
-    print "Unpacking " + zip_name + "<br/>"
-
-    if not zipfile.is_zipfile(zip_name):
-        return False, "Not a valid zip file"
-
-    fh = open(zip_name, 'rb')
-    z = zipfile.ZipFile(fh)
-
-    try:
-        z.getinfo("manifest")
-    except KeyError:
-        fh.close()
-        return False, "Not a TXT app!"
-
-    print "Extracting to " + appdir + "<br/>"
-
-    if os.path.exists(appdir):
-        print "Target dir exists, overwriting contents.<br/>"
-    else:
-        print "Target dir does not exist, creating it.<br/>"
-        os.makedirs(appdir)
-
-    for name in z.namelist():
-        print "Extracting " + name + " ...<br/>"
-        z.extract(name, appdir)
-    fh.close()
-
-    return True, ""
-
-print "<p>Running as " + getpass.getuser() + "</p>"
-print "<p>"
-
-[ok, zip_name] = save_uploaded_file()
-if ok:
-    appdir = appbase + "/" + os.path.splitext(os.path.basename(zip_name))[0]
-    [ok, result_str] = unzip_uploaded_file(zip_name, appdir)
-
-    if not ok:
-        print "<h1>Error: " + result_str + "</h1>"
-    
-    if os.path.isfile(zip_name):
-        print "Deleting " + zip_name + "<br/>"
-        os.remove(zip_name)
-
-    # make the executable executable
-    manifestfile = appdir + "/manifest"
-    if os.path.isfile(manifestfile):
-        manifest = ConfigParser.RawConfigParser()
-        manifest.read(manifestfile)
-
-        # get various fields from manifest
-        executable = appdir + "/" + manifest.get('app', 'exec')
-        
-        print "Making executable: " + executable + "<br/>"
-        os.chmod(executable, 0744)
-
-    # finally send gui a request to refresh its icons
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        print "Requesting rescan ...</br>"
-        # Connect to server and send data
-        sock.connect(("localhost", 9000))
-        sock.sendall("rescan\n")
-    except socket.error, msg:
-        print "Unable to connect to Launcher: ", msg
-
-    finally:
-        sock.close()
-
+user = getpass.getuser()
+print 'Welcome to ft-community-TXT-Frimware-Generation and Update-Kit'
+print 'Be sure your internet connection is workin'
+print 'Current User: ' + user
+if user != "root":
+	print 'Not logged in as root'
+	print 'Script ended'
+	print 'Try again as root'
+	sys.exit()
 else:
-    print "<h1>Error: " + zip_name + "</h1>"
+	print 'You are logged in as root'
+	print 'Script will be continued'
 
-print "</p>"
-print "<p>"
+x = "N"
+while x == "N":
+	print 'Current medias'
+	os.system("lsblk")
+	print 'Please enter path to SD-card without numbers at the end. exp.: /dev/sdb:'
+	sd_card = raw_input("Path to SD: ")
+	print 'Path to SD-card: ' + sd_card
+	print 'Is this right? [J/N]'
+	x = raw_input("[J/N]: ")
 
-print '<form action="index.html">'
-print '<input type="submit" value="Ok">'
-print '</form>'
+print 'Be sure that al prtitions of SD-card are unmounted. Unmount them by typing: umount [mount-point]'
+print 'Are all partitions unmounted?'
+os.system("lsblk")
+x = raw_input("[J/N]: ")
+if x == "N":
+	sys.exit()
 
-print "</p>"
-print "</body></html>"
+print 'Installing packages!'
+os.system("apt-get -y install git mercurial")
+print 'Update repository!'
+os.system("git pull")
+print 'Compile OS!'
+i2c_tools_exist = os.path.exists("dl/i2c-tools-3.1.2.tar.bz2")
+if i2c_tools_exist == False:
+	os.system("wget -P dl http://sources.buildroot.net/i2c-tools-3.1.2.tar.bz2")
+os.system("make fischertechnik_TXT_defconfig")
+os.system("BR2_JLEVEL=$(($(nproc)+1)) make")
+
+print 'Buildroot finished!'
+print 'Formatting SD-card!'
+print 'Answer all questions with Y'
+
+os.system("parted " + sd_card + " mklabel msdos")
+os.system("parted -a optimal " + sd_card + " mkpart primary fat32 1MB 33MB")
+os.system("parted -a optimal " + sd_card + " mkpart primary ext2 34MB 100%")
+os.system("parted " + sd_card + " set 1 boot on")
+os.system("mkfs.vfat -n BOOT " + sd_card + "1")
+os.system("mkfs.ext4 -L ROOT " + sd_card + "2")
+
+print 'Formatting completed!'
+#creating mountpoint
+os.system("mkdir mnt")
+
+os.system("mount " + sd_card + "1 mnt")
+os.system("cp output/images/uImage output/images/am335x-kno_txt.dtb mnt/")
+os.system("umount mnt")
+
+os.system("mount " + sd_card + "2 mnt")
+os.system("tar xvf output/images/rootfs.tar -C mnt/")
+os.system("umount mnt")
+
+print 'Everything is finished. Plug-out the SD-card, put it into the TXT and be happy!'
