fbbb7ed392b92681c3d34871dbeb423230d9f43a
Richard Kunze <richard.kunze@web.de>
2016-05-03 22:12:20 +0200

    Get the firmware version number from git if possible
    If the firmware is built from a git checkout, amend the version numer in /etc/fw-ver.txt with output from 'git describe'. This allows to identify the exact commit from which the firmware has been built, which should be helpful for support.
    
    If the build dir is not under git control (e.g. because the firmware has been built from a source tarball), use the base version number from the board config.

--- board/fischertechnik/TXT/post-build.sh
+++ board/fischertechnik/TXT/post-build.sh
@@ -12,4 +12,21 @@ if [ "$FTC_TXT_CUSTOM" != "" ]; then
 fi
 # Remove obsolete PM firmware load script
 rm -f "$TARGET/etc/init.d/S93-am335x-pm-firmware-load"
+# Try to generate a detailed firmware version number from git.
+# Fall back to the generic version number from the board config
+# if we are not building from a git repository, and abort the build
+# if we are building from git but the tag part of the version 
+# number from git does not match the base version in 
+# board/fischertechnik/TXT/rootfs/etc/fw-ver.txt
+GIT_VERSION=$(git describe --tags --match='v*' 2>/dev/null)
+if [ -n "$GIT_VERSION" ] ; then
+  BASE_VERSION=$(cat board/fischertechnik/TXT/rootfs/etc/fw-ver.txt)
+  if [ "v$BASE_VERSION" = "${GIT_VERSION%%-*}" ] ; then
+    echo ${GIT_VERSION#v*} > $TARGET/etc/fw-ver.txt
+  else
+    echo "Version number $GIT_VERSION from 'git describe' does not match the base version $BASE_VERSION"
+    echo "Please fix the base version in board/fischertechnik/TXT/rootfs/etc/fw-ver.txt"
+    exit 1
+  fi
+fi
 echo "done"
