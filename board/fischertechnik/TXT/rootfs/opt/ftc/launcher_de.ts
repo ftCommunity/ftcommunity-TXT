ded74f7dcfb6254aa3be9d5711ba273369d9e7a4
Till Harbaum <till@harbaum.org>
2016-12-12 09:59:25 +0100

    I18n of launcher

---
+++ board/fischertechnik/TXT/rootfs/opt/ftc/launcher_de.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>Category</name>
+    <message>
+        <location filename="launcher.py" line="43"/>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <location filename="launcher.py" line="45"/>
+        <source>Models</source>
+        <translation>Modelle</translation>
+    </message>
+    <message>
+        <location filename="launcher.py" line="47"/>
+        <source>Tools</source>
+        <translation>Werzeuge</translation>
+    </message>
+    <message>
+        <location filename="launcher.py" line="51"/>
+        <source>Demos</source>
+        <translation>Demos</translation>
+    </message>
+    <message>
+        <location filename="launcher.py" line="970"/>
+        <source>All</source>
+        <translation>Alle</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="launcher.py" line="126"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="launcher.py" line="132"/>
+        <source>Cancel</source>
+        <translation>Abbruch</translation>
+    </message>
+</context>
+</TS>
