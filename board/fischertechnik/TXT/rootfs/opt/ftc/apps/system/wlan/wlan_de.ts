9aa6ec41073a1341d4e70a55ab5e053a93e49410
Till Harbaum <till@harbaum.org>
2016-12-16 09:54:02 +0100

    Rename WiFi app to WLAN and i18n it

---
+++ board/fischertechnik/TXT/rootfs/opt/ftc/apps/system/wlan/wlan_de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="wlan.py" line="257"/>
+        <source>WLAN</source>
+        <translation>WLAN</translation>
+    </message>
+    <message>
+        <location filename="wlan.py" line="293"/>
+        <source>Encryption:</source>
+        <translation>Verschlüsselung:</translation>
+    </message>
+    <message>
+        <location filename="wlan.py" line="405"/>
+        <source>Connect</source>
+        <translation>Verbinde</translation>
+    </message>
+    <message>
+        <location filename="wlan.py" line="374"/>
+        <source>Key</source>
+        <translation>Schlüssel</translation>
+    </message>
+    <message>
+        <location filename="wlan.py" line="402"/>
+        <source>connected</source>
+        <translation>verbunden</translation>
+    </message>
+</context>
+<context>
+    <name>placeholder</name>
+    <message>
+        <location filename="wlan.py" line="303"/>
+        <source>key</source>
+        <translation>Schlüssel</translation>
+    </message>
+</context>
+</TS>
