5fe2477c0cf0d301de05c9a73bdf14b622e57e11
Raphael Jacob <r.jacob2002@gmail.com>
2017-03-04 15:08:50 +0100

    Update update Plugin and add german translation file

--- board/fischertechnik/TXT/rootfs/opt/ftc/plugins/update.py
+++ board/fischertechnik/TXT/rootfs/opt/ftc/plugins/update.py
@@ -1,15 +1,25 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+from pathlib import Path
 import urllib.request
 import json
 import ssl
 import time
 import configparser
+import semantic_version
+from PyQt4.QtCore import QCoreApplication
 
-name = 'Update'
 cachefile = '/tmp/update_plugin.cache'
 get_interval = 86400.0  # one day
+global next_get
+next_get = None
+global cache_rel_ver
+cache_rel_ver = None
+
+
+def name():
+    return QCoreApplication.translate("PluginSysUpdate", "Update")
 
 
 def get_release_version():
@@ -19,7 +29,7 @@ def get_release_version():
         ctx.verify_mode = ssl.CERT_NONE
         raw_data = urllib.request.urlopen('https://api.github.com/repos/ftCommunity/ftcommunity-TXT/releases/latest', context=ctx).read().decode()
         latest_release = json.loads(raw_data)
-        release_version = latest_release['tag_name'].replace('v', '')
+        release_version = semantic_version.Version(latest_release['tag_name'].replace('v', ''))
         return(release_version)
     except:
         return(None)
@@ -27,45 +37,36 @@ def get_release_version():
 
 def get_current_version():
     try:
-        fw_file = open('/etc/fw-ver.txt', 'r')
-        current_version = fw_file.read().strip().split('-')[0]
-        fw_file.close()
-        return(current_version)
+        current = semantic_version.Version(Path('/etc/fw-ver.txt').read_text())
+        return(semantic_version.Version(to_str(current)))
     except:
         return(None)
 
 
+def to_str(ver):
+    return(str(ver.major) + "." + str(ver.minor) + "." + str(ver.patch))
+
+
 def update_avaible():
-    try:
-        cache = configparser.SafeConfigParser()
-        cache.read(cachefile)
-        next_get = cache.get('general', 'next_get')
-        release_version = cache.get('general', 'release_version')
-        if time.time() > float(next_get):
+    global next_get
+    global cache_rel_ver
+    if cache_rel_ver != None:
+        release_version = cache_rel_ver
+        if time.time() > next_get:
             release_version = get_release_version()
-            next_get = str(time.time() + get_interval)
+            next_get = time.time() + get_interval
             if release_version == None:
                 return(None, None, None)
-    except:
+    else:
         release_version = get_release_version()
-        next_get = str(time.time() + get_interval)
+        next_get = time.time() + get_interval
         if release_version == None:
             return(None, None, None)
     current_version = get_current_version()
     if current_version == None:
         return(None, None, None)
-
-    try:
-        cache_w = configparser.ConfigParser()
-        cache_file_w = open(cachefile, 'w')
-        cache_w.add_section('general')
-        cache_w.set('general', 'next_get', str(next_get))
-        cache_w.set('general', 'release_version', release_version)
-        cache_w.write(cache_file_w)
-        cache_file_w.close()
-    except:
-        return(None, None, None)
-    return(release_version != current_version, release_version, current_version)
+    cache_rel_ver = release_version
+    return(current_version < release_version, release_version, current_version)
 
 
 def icon():
@@ -88,7 +89,6 @@ def icon():
                  '                ']
     update_status, update_version, current_version = update_avaible()
     if update_status == True:
-        icon_data[2] = '. c green'
         return(icon_data)
     elif update_status == False:
         return(None)
@@ -100,8 +100,8 @@ def icon():
 def status():
     update_status, update_version, current_version = update_avaible()
     if update_status == True:
-        return('Available V ' + update_version + '\nto update open the Settings App')
+        return(QCoreApplication.translate("PluginSysUpdate", "Available V") + to_str(update_version))
     elif update_status == False:
-        return('Up-to-date V ' + current_version)
+        return(QCoreApplication.translate("PluginSysUpdate", "Up-to-date V") + to_str(current_version))
     else:
-        return('An error occurred')
+        return(QCoreApplication.translate("PluginSysUpdate", "An error occurred"))
---
+++ board/fischertechnik/TXT/rootfs/opt/ftc/plugins/update_de.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>PluginSysUpdate</name>
+    <message>
+        <location filename="update.py" line="22"/>
+        <source>Update</source>
+        <translation>Update</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="104"/>
+        <source>Available V</source>
+        <translation>Verfügbar V</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="106"/>
+        <source>Up-to-date V</source>
+        <translation>Aktuell V</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="108"/>
+        <source>An error occurred</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+</TS>
