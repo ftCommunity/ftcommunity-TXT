9d1acee0aa134cbf34c8cb019b6f26e3c6447ea3
PeterDHabermehl <peter.habermehl@gmx.de>
2017-03-19 22:32:01 +0100

    Bugfix in TouchAuxFTCamPhotoRequester -> release camera after taking photo.
    Signed-off-by: PeterDHabermehl <peter.habermehl@gmx.de>

--- board/fischertechnik/TXT/rootfs/opt/ftc/TouchAuxiliary.py
+++ board/fischertechnik/TXT/rootfs/opt/ftc/TouchAuxiliary.py
@@ -195,6 +195,7 @@ class TouchAuxFTCamPhotoRequester(TouchDialog):
     
     def on_photo(self):
         self.img=self.cw.getPhoto() 
+        self.cw.closeCam()
         self.close()
         
     def exec_(self):
@@ -259,7 +260,9 @@ class TouchAuxCamWidget(QWidget):
 
     def heightForWidth(self,w):
         return w*3/4
-        
+    
+    def closeCam(self):
+        self.cap.release()    
     
     def grab(self):
         self.frame = self.cap.read()[1]
