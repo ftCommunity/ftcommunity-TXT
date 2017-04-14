381235589a422ee044d0b275a00535a00abf83f9
Raphael Jacob <r.jacob2002@gmail.com>
2017-04-14 23:32:52 +0200

    Add some new raw translations

--- board/fischertechnik/TXT/rootfs/opt/ftc/apps/system/update/update_de.ts
+++ board/fischertechnik/TXT/rootfs/opt/ftc/apps/system/update/update_de.ts
@@ -1,92 +1,116 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="de_DE" sourcelanguage="en_US">
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en_US">
 <context>
     <name>ErrorCodes</name>
     <message>
-        <location filename="update.py" line="104"/>
+        <location filename="update.py" line="109"/>
         <source>Download validation failed!</source>
         <translation>Download Validierung fehlgeschlagen!</translation>
     </message>
     <message>
-        <location filename="update.py" line="105"/>
+        <location filename="update.py" line="110"/>
         <source>Backup failed!</source>
         <translation>Sicherung fehlgeschlagen!</translation>
     </message>
     <message>
-        <location filename="update.py" line="106"/>
+        <location filename="update.py" line="111"/>
         <source>Installation failed!</source>
         <translation>Installation fehlgeschlagen!</translation>
     </message>
     <message>
-        <location filename="update.py" line="110"/>
+        <location filename="update.py" line="115"/>
         <source>Unknown Error!</source>
         <translation>Unbekannter Fehler!</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="104"/>
+        <source>Up-to-date!</source>
+        <translation>Bereits aktuell!</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="106"/>
+        <source>System Files Error!</source>
+        <translation>Systemdateien Fehler!</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="107"/>
+        <source>Download Error!</source>
+        <translation>Download Fehler!</translation>
     </message>
 </context>
 <context>
     <name>ErrorDialog</name>
     <message>
-        <location filename="update.py" line="112"/>
+        <location filename="update.py" line="117"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="update.py" line="115"/>
+        <location filename="update.py" line="120"/>
         <source>TXT will reboot soon!</source>
         <translation>Der TXT startet wird glecih neu starten!</translation>
     </message>
     <message>
-        <location filename="update.py" line="116"/>
+        <location filename="update.py" line="121"/>
         <source>Finished</source>
         <translation>Erfolgreich</translation>
     </message>
 </context>
 <context>
+    <name>OkDialog</name>
+    <message>
+        <location filename="update.py" line="320"/>
+        <source>Confirm</source>
+        <translation>Abfrage</translation>
+    </message>
+</context>
+<context>
     <name>ProgressDialog</name>
     <message>
-        <location filename="update.py" line="152"/>
+        <location filename="update.py" line="157"/>
         <source>Progress</source>
         <translation>Fortschritt</translation>
     </message>
     <message>
-        <location filename="update.py" line="155"/>
+        <location filename="update.py" line="160"/>
         <source>Initializing Update</source>
         <translation>Intialisiere Update</translation>
     </message>
     <message>
-        <location filename="update.py" line="187"/>
+        <location filename="update.py" line="192"/>
         <source>Preparation</source>
         <translation>
 
 Vorbereitung</translation>
     </message>
     <message>
-        <location filename="update.py" line="197"/>
+        <location filename="update.py" line="202"/>
         <source>Pending</source>
         <translation>Ausstehen</translation>
     </message>
     <message>
-        <location filename="update.py" line="190"/>
+        <location filename="update.py" line="195"/>
         <source>Download</source>
         <translation>Download</translation>
     </message>
     <message>
-        <location filename="update.py" line="193"/>
+        <location filename="update.py" line="198"/>
         <source>Backup</source>
         <translation>Sicherung</translation>
     </message>
     <message>
-        <location filename="update.py" line="212"/>
+        <location filename="update.py" line="217"/>
         <source>Validating...</source>
         <translation>Validierung...</translation>
     </message>
     <message>
-        <location filename="update.py" line="230"/>
+        <location filename="update.py" line="235"/>
         <source>Done</source>
         <translation>Fertig</translation>
     </message>
     <message>
-        <location filename="update.py" line="196"/>
+        <location filename="update.py" line="201"/>
         <source>Installation</source>
         <translation>Installation</translation>
     </message>
@@ -94,14 +118,19 @@ Vorbereitung</translation>
 <context>
     <name>TouchGuiApplication</name>
     <message>
-        <location filename="update.py" line="303"/>
+        <location filename="update.py" line="360"/>
         <source>Update</source>
         <translation>Update</translation>
     </message>
     <message>
-        <location filename="update.py" line="309"/>
+        <location filename="update.py" line="366"/>
         <source>Searching for updates</source>
         <translation>Suche nach Updates</translation>
+    </message>
+    <message>
+        <location filename="update.py" line="428"/>
+        <source>Select one release of the list below:</source>
+        <translation>Wähle eine Version aus der Liste:</translation>
     </message>
 </context>
 </TS>
