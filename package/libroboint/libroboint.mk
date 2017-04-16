bb980643d79c3255d65ad19c5e5d4f444d9594ba
Raphael Jacob <r.jacob2002@gmail.com>
2017-04-16 23:40:54 +0200

    Use cmake env

--- package/libroboint/libroboint.mk
+++ package/libroboint/libroboint.mk
@@ -8,5 +8,6 @@ LIBROBOINT_VERSION = f99ca1204af1ffcd89bf5b0c0195590738bbb451
 LIBROBOINT_SITE = $(call github,nxdefiant,libroboint,$(LIBROBOINT_VERSION))
 LIBROBOINT_LICENSE = LGPLv2.1
 LIBROBOINT_DEPENDENCIES = libusb libusb-compat
+LIBROBOINT_CONF_OPTS = -DBUILD_DEMOS=ON
 
-$(ls)
+$(eval $(cmake-package))
