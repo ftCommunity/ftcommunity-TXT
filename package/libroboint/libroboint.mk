71f6263d303095341d8a6457faf907372f838cb4
Raphael Jacob <r.jacob2002@gmail.com>
2017-04-16 23:18:08 +0200

    Some first test for a libroboint package

---
+++ package/libroboint/libroboint.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# libroboint
+#
+################################################################################
+
+LIBROBOINT_VERSION = f99ca1204af1ffcd89bf5b0c0195590738bbb451
+LIBROBOINT_SITE = $(call github,nxdefiant,libroboint,$(LIBROBOINT_VERSION))
+LIBROBOINT_LICENSE = LGPLv2.1
+LIBROBOINT_DEPENDENCIES = libusb libusb-compat
+
+$(ls)
