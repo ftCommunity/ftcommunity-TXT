af9074d10ad1acc85889cb921446cb94fb9bb92e
Raphael Jacob <r.jacob2002@gmail.com>
2017-04-17 10:58:51 +0200

    Bump python-libroboint to c7e77a991df1b5153b94893936f6d5f4762c44d9

--- package/python-libroboint/python-libroboint.mk
+++ package/python-libroboint/python-libroboint.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBROBOINT_VERSION = 74db52ecc90c37286024a6dd30cd402f80c080db
+PYTHON_LIBROBOINT_VERSION = c7e77a991df1b5153b94893936f6d5f4762c44d9
 PYTHON_LIBROBOINT_SITE = $(call github,PeterDHabermehl,libroboint-py3,$(PYTHON_LIBROBOINT_VERSION))
 PYTHON_LIBROBOINT_DEPENDENCIES = libroboint
 
