dcc46556fccb547841e116e09558dee67e8519c8
Raphael Jacob <r.jacob2002@gmail.com>
2017-04-28 20:22:17 +0200

    Add uclibc to libroboint

--- package/libroboint/libroboint.mk
+++ package/libroboint/libroboint.mk
@@ -7,7 +7,7 @@
 LIBROBOINT_VERSION = f99ca1204af1ffcd89bf5b0c0195590738bbb451
 LIBROBOINT_SITE = $(call github,nxdefiant,libroboint,$(LIBROBOINT_VERSION))
 LIBROBOINT_LICENSE = LGPLv2.1
-LIBROBOINT_DEPENDENCIES = libusb host-libusb libusb-compat host-libusb-compat
+LIBROBOINT_DEPENDENCIES = libusb host-libusb libusb-compat host-libusb-compat uclibc
 LIBROBOINT_CONF_OPTS = -DBUILD_DEMOS=ON
 LIBROBOINT_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) LIB_LDCONFIG_CMD=true install
 
