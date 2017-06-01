445cd3444c8c5db01f2e2ba63751be519644a2a2
Till Harbaum <till@harbaum.org>
2017-06-01 16:20:35 +0200

    Re-Add netreq tool after accidentally dropping it in 865d5d393a0c51acb0a00f30fecfc65f4696a854

---
+++ package/netreq/netreq.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# netreq
+#
+################################################################################
+
+NETREQ_SITE = ./package/netreq
+NETREQ_SITE_METHOD = local
+NETREQ_DEPENDENCIES = libnetfilter_queue
+
+define NETREQ_BUILD_CMDS
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define NETREQ_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/netreq $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
