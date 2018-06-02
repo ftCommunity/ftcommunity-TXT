63af3b3911cb05603f20cb3066cc8184ae15c0ba
Richard Kunze <richard.kunze@web.de>
2018-06-02 17:46:19 +0200

    Explicitly set the touchscreen device for ts_calibrate
    Newer versions of ts_calibrate cannot autodectect the am335x touchscreen
    anymore but need to have the device specified via the TSLIB_TSDEVICE
    environment variable.
    
    Fixes #120

--- board/fischertechnik/TXT/rootfs/opt/ftc/apps/system/display/display.py
+++ board/fischertechnik/TXT/rootfs/opt/ftc/apps/system/display/display.py
@@ -49,7 +49,7 @@ class DisplaySettingsPlugin(LauncherPlugin):
         self.mainWindow = TouchBaseWidget()
         self.mainWindow.show()
         old_window.close()
-        subprocess.run(["sudo", "/usr/bin/ts_calibrate"])
+        subprocess.run(["sudo", "TSLIB_TSDEVICE=/dev/input/event0", "/usr/bin/ts_calibrate"])
         self.restart_launcher(QCoreApplication.translate("main", "Activating new touchscreen calibration..."))
 
     def on_change_orientation(self, index):
