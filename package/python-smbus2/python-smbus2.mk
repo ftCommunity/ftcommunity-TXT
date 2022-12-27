6a8d22640d87454d08a3b6f8507a0338bc925ff1
Raphael Jacob <r.jacob2002@gmail.com>
2022-12-27 13:24:14 +0100

    Update python-smbus2 (#215)

--- package/python-smbus2/python-smbus2.mk
+++ package/python-smbus2/python-smbus2.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SMBUS2_VERSION = 0.2.3
+PYTHON_SMBUS2_VERSION = 0.4.2
 PYTHON_SMBUS2_SOURCE = smbus2-$(PYTHON_SMBUS2_VERSION).tar.gz
-PYTHON_SMBUS2_SITE = https://files.pythonhosted.org/packages/20/21/fd4e359bd130c09a728c8549fccabf3be70ea08ca7c7c6b5822faf97dcf3
+PYTHON_SMBUS2_SITE = https://files.pythonhosted.org/packages/7c/01/18a9c3fccc2ddc0af16ddbe52aadc4585fbd1e7ae4ee32e780abbfc7fc97
 PYTHON_SMBUS2_SETUP_TYPE = setuptools
 PYTHON_SMBUS2_LICENSE = MIT
 
