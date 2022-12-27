688ed23369bcf4c7e8d622333260157fcab4e28f
Raphael Jacob <r.jacob2002@gmail.com>
2022-12-27 13:22:16 +0100

    Update python-luma* (#215)

--- package/python-lumacore/python-lumacore.mk
+++ package/python-lumacore/python-lumacore.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LUMACORE_VERSION = 1.8.3
+PYTHON_LUMACORE_VERSION = 2.4.0
 PYTHON_LUMACORE_SOURCE = luma.core-$(PYTHON_LUMACORE_VERSION).tar.gz
-PYTHON_LUMACORE_SITE = https://files.pythonhosted.org/packages/88/33/9be76a139d914d2a1ec9593632d81dd277d4b7005554a9881221d2b8e3ad
+PYTHON_LUMACORE_SITE = https://files.pythonhosted.org/packages/b5/d0/dd025f8f665024ec8a0aab928fcb5fb766980f83b1d0127211ccee01054b
 PYTHON_LUMACORE_SETUP_TYPE = setuptools
 PYTHON_LUMACORE_LICENSE = MIT
 PYTHON_LUMACORE_LICENSE_FILES = LICENSE.rst
--- package/python-lumaled_matrix/python-lumaled_matrix.mk
+++ package/python-lumaled_matrix/python-lumaled_matrix.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LUMALED_MATRIX_VERSION = 1.1.1
+PYTHON_LUMALED_MATRIX_VERSION = 1.7.0
 PYTHON_LUMALED_MATRIX_SOURCE = luma.led_matrix-$(PYTHON_LUMALED_MATRIX_VERSION).tar.gz
-PYTHON_LUMALED_MATRIX_SITE = https://files.pythonhosted.org/packages/43/ad/592dff62a2962a73674e714eddd0e388758030d5b82cb88999adca0c0e89
+PYTHON_LUMALED_MATRIX_SITE = https://files.pythonhosted.org/packages/cc/41/8cf7078e77da1ededef2bcc958114afc52725e948b01f16fa3542c914bbd
 PYTHON_LUMALED_MATRIX_SETUP_TYPE = setuptools
 PYTHON_LUMALED_MATRIX_LICENSE = MIT
 PYTHON_LUMALED_MATRIX_LICENSE_FILES = LICENSE.rst
