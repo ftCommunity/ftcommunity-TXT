1f3adefe96d1c7ec1bf4064593d999f9e1b75b40
Raphael Jacob <r.jacob2002@gmail.com>
2022-12-28 20:39:06 +0100

    Update libroboint, python-libroboint (#215)

--- package/python-libroboint/python-libroboint.mk
+++ package/python-libroboint/python-libroboint.mk
@@ -4,14 +4,10 @@
 #
 ################################################################################
 
-PYTHON_LIBROBOINT_VERSION = b4dda4e5bad4f828096f44904c98f597b7c13be3
-PYTHON_LIBROBOINT_SITE = $(call github,PeterDHabermehl,libroboint-py3,$(PYTHON_LIBROBOINT_VERSION))
-PYTHON_LIBROBOINT_DEPENDENCIES = libroboint
+PYTHON_LIBROBOINT_VERSION = cf25fdba52ad1343317afca683d8b272682f0077
+PYTHON_LIBROBOINT_SITE = $(call gitlab,Humpelstilzchen,libroboint,$(LIBROBOINT_VERSION))
+PYTHON_LIBROBOINT_LICENSE = LGPLv2.1
+PYTHON_LIBROBOINT_SUBDIR = python
+PYTHON_LIBROBOINT_SETUP_TYPE = distutils
 
-define PYTHON_LIBROBOINT_INSTALL_PY_STUFF
-	$(INSTALL) -D -m 0644 $(@D)/robointerface.py $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/robointerface.py
-endef
-PYTHON_LIBROBOINT_POST_INSTALL_TARGET_HOOKS += PYTHON_LIBROBOINT_INSTALL_PY_STUFF
-
-$(eval $(generic-package))
-
+$(eval $(python-package))
