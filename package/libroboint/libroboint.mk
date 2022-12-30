975693f2b07c57fe935f6f43cd192e33875f9211
Raphael Jacob <r.jacob2002@gmail.com>
2022-12-31 00:37:03 +0100

    try to bundle libroboint/python-libroboint

--- package/libroboint/libroboint.mk
+++ package/libroboint/libroboint.mk
@@ -9,5 +9,12 @@ LIBROBOINT_SITE = $(call gitlab,Humpelstilzchen,libroboint,$(LIBROBOINT_VERSION)
 LIBROBOINT_LICENSE = LGPLv2.1
 LIBROBOINT_DEPENDENCIES = libusb host-libusb libusb-compat host-libusb-compat binutils
 LIBROBOINT_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) LIB_LDCONFIG_CMD=true install
+LIBROBOINT_MAKE_OPTS = all
+LIBROBOINT_MAKE_ENV = PYTHON_ROOT=$(TARGET_DIR)
+
+ifeq ($(BR2_PACKAGE_LIBROBOINT_PYTHON),y)
+	LIBROBOINT_DEPENDENCIES += host-python3 python3
+	LIBROBOINT_MAKE_OPTS += python
+endif
 
 $(eval $(cmake-package))
