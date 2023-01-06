0d45297cf7432f3ddb1bc4b3eb1cae350ec16352
Raphael Jacob <r.jacob2002@gmail.com>
2023-01-06 02:11:59 +0100

    update libroboint for libusb1

--- package/libroboint/libroboint.mk
+++ package/libroboint/libroboint.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBROBOINT_VERSION = cf25fdba52ad1343317afca683d8b272682f0077
+LIBROBOINT_VERSION = 9d7cfb09386f0f63b3954fdf4043b92b259548e4
 LIBROBOINT_SITE = $(call gitlab,Humpelstilzchen,libroboint,$(LIBROBOINT_VERSION))
 LIBROBOINT_LICENSE = LGPLv2.1
-LIBROBOINT_DEPENDENCIES = libusb host-libusb libusb-compat host-libusb-compat binutils
+LIBROBOINT_DEPENDENCIES = libusb
 LIBROBOINT_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) LIB_LDCONFIG_CMD=true install
 LIBROBOINT_MAKE_OPTS = all
 LIBROBOINT_MAKE_ENV = PYTHON_ROOT=$(TARGET_DIR)
