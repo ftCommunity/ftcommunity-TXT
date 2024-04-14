4f02f130fc28f793a1ef49e448efd6e76b7023ef
Ralf Dragon <hypnotoad@lindra.de>
2024-04-14 11:28:59 +0200

    Disable lighthttpd's ssi module
    The module fails with an error due to "read" not being
    implemented. This likely has to do with our old Kernel. In the long
    run, it would be good to have a python-based web server which directly
    calls our python code. This commit also prepares that.

--- board/fischertechnik/TXT/rootfs/var/www/applist.py
+++ board/fischertechnik/TXT/rootfs/var/www/applist.py
@@ -8,23 +8,7 @@ import sys, os, socket
 base = "/opt/ftc"
 
 cols = 5   # 3 is same as on TXT itself
-count = 0
 
-# request executable name of currently running app from 
-# launcher
-current_executable = ""
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    # Connect to server and send data
-    sock.connect(("localhost", 9000))
-    sock.sendall(bytes("get-app\n", "UTF-8"))
-    current_executable = sock.makefile().readline().strip()
-except socket.error as msg:
-    print('<h2><font color="red">Launcher not responding!</font></h2>')
-finally:
-    sock.close()
-
-print('<table cellspacing="20">')
 
 # return a list of directories containing apps
 # searches under /opt/ftc/apps/<group>/<app>
@@ -59,56 +43,78 @@ def scan_app_dirs():
     # return a list of only the directories of the now sorted list
     return ([x[1] for x in app_dirs])
 
-# find all manifest files in the apps directory
-for app_dir in scan_app_dirs():
-    manifestfile = os.path.join(app_dir, "manifest")
-    manifest = configparser.RawConfigParser()
-    manifest.read_file(open(manifestfile, "r", encoding="utf8"))
+def print_apptable():
 
-    # get various fields from manifest
-    appname = manifest.get('app', 'name')
-    if manifest.has_option('app', 'desc'):
-        description = manifest.get('app', 'desc')
-    else:
-        description = "no desc"
-    # the icon name is a little tricky as the web server accesses files
-    # relative to its document root
-    group_dir, app_dir_name = os.path.split(app_dir)
-    group_dir_name = os.path.basename(group_dir)
-    if manifest.has_option('app', 'icon'):
-        iconname = os.path.join( "apps", group_dir_name, app_dir_name, manifest.get('app', 'icon'))
-    else:
-        iconname = "icon.png"
+    # request executable name of currently running app from 
+    # launcher
+    current_executable = ""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # Connect to server and send data
+        sock.connect(("localhost", 9000))
+        sock.sendall(bytes("get-app\n", "UTF-8"))
+        current_executable = sock.makefile().readline().strip()
+    except socket.error as msg:
+        print('<h2><font color="red">Launcher not responding!</font></h2>')
+    finally:
+        sock.close()
 
-    if manifest.has_option('app', 'exec'):
-        exec_relative = os.path.join(group_dir_name, app_dir_name, manifest.get('app', 'exec'))
-    else:
-        exec_relative = ''
-    is_running = current_executable == exec_relative
+    print('<table cellspacing="20">')
 
-    if count == 0:
-        print("<tr>")
+    # find all manifest files in the apps directory
+    count = 0
+    for app_dir in scan_app_dirs():
+        manifestfile = os.path.join(app_dir, "manifest")
+        manifest = configparser.RawConfigParser()
+        manifest.read_file(open(manifestfile, "r", encoding="utf8"))
 
-    print('<td align="center"', end="")
-    if is_running: print(' bgcolor="green"', end="")
-    print('>')
-    print('<div title="' + description + '">')
-    manifestfile_enc = manifestfile.replace(' ','%20')
-    print('<a href="appinfo.py?manifest=' + manifestfile_enc + '">')
-    print('<img src="' + iconname + '"><br>')
-    print(appname)
-    print('</a></div></td>')
-    count += 1
-    if count == cols:
-        print("</tr>")
-        count = 0
+        # get various fields from manifest
+        appname = manifest.get('app', 'name')
+        if manifest.has_option('app', 'desc'):
+            description = manifest.get('app', 'desc')
+        else:
+            description = "no desc"
+        # the icon name is a little tricky as the web server accesses files
+        # relative to its document root
+        group_dir, app_dir_name = os.path.split(app_dir)
+        group_dir_name = os.path.basename(group_dir)
+        if manifest.has_option('app', 'icon'):
+            iconname = os.path.join( "apps", group_dir_name, app_dir_name, manifest.get('app', 'icon'))
+        else:
+            iconname = "icon.png"
 
-# fill columns
-while count != 0:
-    print("<td></td>")
-    count += 1
-    if count == cols:
-        print("</tr>")
-        count = 0
+        if manifest.has_option('app', 'exec'):
+            exec_relative = os.path.join(group_dir_name, app_dir_name, manifest.get('app', 'exec'))
+        else:
+            exec_relative = ''
+        is_running = current_executable == exec_relative
+
+        if count == 0:
+            print("<tr>")
+
+        print('<td align="center"', end="")
+        if is_running: print(' bgcolor="green"', end="")
+        print('>')
+        print('<div title="' + description + '">')
+        manifestfile_enc = manifestfile.replace(' ','%20')
+        print('<a href="appinfo.py?manifest=' + manifestfile_enc + '">')
+        print('<img src="' + iconname + '"><br>')
+        print(appname)
+        print('</a></div></td>')
+        count += 1
+        if count == cols:
+            print("</tr>")
+            count = 0
+
+    # fill columns
+    while count != 0:
+        print("<td></td>")
+        count += 1
+        if count == cols:
+            print("</tr>")
+            count = 0
         
-print("</table>")
+    print("</table>")
+
+if __name__ == "__main__":
+    print_apptable()
